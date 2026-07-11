52ae3a8d5830f54f333febfacaead5b138928ce3
Akshay Solanki <146133555+1solanki1@users.noreply.github.com>
2026-07-11 14:19:21 +0530

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+int n=nums.size();
+vector<int>ans(n);
+
+for(int i=0;i<n;i++){
+    int pro=1;
+    for(int j=0;j<n;j++){
+    if(i!=j){ pro*=nums[j];}
+    }
+
+
+ans[i]=pro;
+}
+    return ans;
+
+}   
+};
